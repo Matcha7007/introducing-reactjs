d6a16a2a8e94fa5b202907810b8cd6e6ca4ab0a5
Matcha7007 <matchaaddict007@gmail.com>
2023-03-29 16:47:19 +0700

    Zustand | port our Redux application to the much simpler Zustand state manager.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createStore } from 'redux';
-import { Provider, useSelector, useDispatch } from 'react-redux';
-
-
 import PokemonInfo from "./components/PokemonInfo";
 import PokemonFilter from "./components/PokemonFilter";
 import PokemonTable from "./components/PokemonTable";
-
 import "./App.css";
 
-const pokemonReducer = (
-  state = {
-    filter: '',
-    pokemon: [],
-    selectedPokemon: null
-  }, 
-  action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return {
-        ...state,
-        filter: action.payload,
-      };
-    case 'SET_POKEMON':
-      return {
-        ...state,
-        pokemon: action.payload,
-      };
-    case 'SET_SELECTED_POKEMON':
-      return {
-        ...state,
-        selectedPokemon: action.payload,
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(pokemonReducer);
 
 const Title = styled.h1`
   text-align: center;
@@ -56,26 +22,6 @@ const TwoColumnLayout = styled.div`
 `;
 
 function App() {
-  const dispatch = useDispatch();
-  const pokemon = useSelector(state => state.pokemon);
-
-  React.useEffect(() => {
-    const fetchPokemon = async () => {
-      const resp = await fetch("http://localhost:3000/pokemon.json");
-      const data = await resp.json();
-      return dispatch({
-        type: 'SET_POKEMON',
-        payload: data
-      });
-    }
-    fetchPokemon();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (!pokemon) {
-    return <div>Loading data</div>;
-  }
-
   return (
     <PageContainer>
       <CssBaseline />
@@ -92,8 +38,4 @@ function App() {
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+export default App
--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import useStore from '../store';
 import PokemonRow from "./PokemonRow";
 
 function PokemonTable() {
-  const dispatch = useDispatch();
-  const pokemon = useSelector(state => state.pokemon);
-  const filter = useSelector(state => state.filter);
+  const pokemon =  useStore((state) => state.pokemon);
+  const filter =  useStore((state) => state.filter);
+  const setSelectedPokemon =  useStore((state) => state.setSelectedPokemon)
   return (
     <table width="100%">
       <tbody>
@@ -18,10 +18,7 @@ function PokemonTable() {
             <PokemonRow
               pokemon={pokemon}
               key={pokemon.id}
-              onClick={(pokemon) => dispatch({
-                type: 'SET_SELECTED_POKEMON',
-                payload: pokemon
-              })}
+              onClick={(pokemon) => setSelectedPokemon(pokemon)}
             />
           ))}
       </tbody>
